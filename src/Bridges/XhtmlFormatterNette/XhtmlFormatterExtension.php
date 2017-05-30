492451ffcfd54431131e3b3e1f9bd6597a880458
Vladimír Macháček <8machy@seznam.cz>
2017-05-30 18:59:21 +0200

    - v0.1 - Added Nette extension

---
+++ src/Bridges/XhtmlFormatterNette/XhtmlFormatterExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ *
+ * This file is part of the Xhtml formatter
+ *
+ * Copyright (c) 2017 Vladimír Macháček
+ *
+ * For the full copyright and license information, please view the file license.md
+ * that was distributed with this source code.
+ *
+ */
+
+declare(strict_types = 1);
+
+
+namespace XhtmlFormatter\Bridges;
+
+use Nette\DI\CompilerExtension;
+
+
+class XhtmlFormatterExtension extends CompilerExtension
+{
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+
+		$builder->addDefinition($this->prefix('formatter'))
+			->setClass('XhtmlFormatter\Formatter');
+	}
+}
